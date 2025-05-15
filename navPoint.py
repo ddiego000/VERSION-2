e02cdf44f28c79bda4301edcb20b3adf0ba2ffbc
ddiego000 <diego.pinon@estudiantat.upc.edu>
2025-05-15 12:16:54 +0200

    Create navPoint.py

---
+++ navPoint.py
@@ -0,0 +1,6 @@
+class NavPoint:
+    def __init__(self, number, name, latitude, longitude):
+        self.number = number
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
